c28afe22143b96643143297959024bbeb9ac0c0b
0Chan-smc <cody@thesmc.co.kr>
2024-05-20 18:33:52 +0900

    feat: 컴포넌트 작성 템플릿 추가

---
+++ src/pages/components/_data.ts
@@ -0,0 +1,93 @@
+import accordionImage from './images/accordion.svg';
+import alertsImage from './images/alerts.svg';
+import avatarImage from './images/avatar.svg';
+import buttonGroupImage from './images/button-group.svg';
+import buttonsImage from './images/buttons.svg';
+import cardsImage from './images/cards.svg';
+import carouselImage from './images/carousel.svg';
+import dropdownImage from './images/dropdown.svg';
+import listGroupImage from './images/list-group.svg';
+import tabsImage from './images/tabs.svg';
+
+export default [
+  {
+    title: '아코디언',
+    Svg: accordionImage,
+    description:
+      'Play the mathimagical game where you arrange monsters to complete the dungeon’s sum-mons. May Trix the Witch is doing all she can to bring the dungeon to order!',
+    url: 'https://excaliburjs.com/ludum-55/',
+    source: 'https://github.com/excaliburjs/ludum-55',
+  },
+  {
+    title: '경고',
+    Svg: alertsImage,
+    description:
+      'A little farming game created with the Excalibur Tiled plugin. Come and enjoy the supremely cozy vibes on this lovely farm.',
+    url: 'https://excali-farm.netlify.app/',
+    source: 'https://github.com/mahbarahona/tiles',
+  },
+  {
+    title: '아바타',
+    Svg: avatarImage,
+    description:
+      "Put your skis and let's race some randomly generated alpine ski tracks with your friends.",
+    url: 'https://une-entreprise.ch/retroski/',
+    source: 'https://github.com/mathieuher/RetroSki',
+  },
+  {
+    title: '버튼 그룹',
+    Svg: buttonGroupImage,
+    description:
+      'Ride your motorcycle through the city at night! Jump over as many vehicles as you can!',
+    url: 'https://nightbike.mattjennin.gs/',
+    source: 'https://github.com/mattjennings/nightbike',
+  },
+  {
+    title: '버튼',
+    Svg: buttonsImage,
+    description:
+      'Super Metronome Hero is a rhythm game. Rather than trying to make a game I wanted to play out of this rather popular genre I decided to send it up with this nonsense joke game.',
+    url: 'https://super-metronome-hero.vidja.games/',
+    source: 'https://github.com/dcgw/super-metronome-hero',
+  },
+  {
+    title: '카드',
+    Svg: cardsImage,
+    description:
+      'This game is an easily customizable template for developing an Excalibur game with Ionic React, the XState library, i18next, Twine integration for dynamic dialogs, and CapacitorJS. It comes with scene transitions, data persistence and tutorials included!',
+    url: 'https://nicastro.in/excalibur-games/sword-adventure',
+    source: 'https://github.com/facondiaGames/sword-adventure',
+  },
+  {
+    title: '캐러셀',
+    Svg: carouselImage,
+    description:
+      'This game was created for Ludum Dare 50. Defend your sandcastle from crabs, turtles, and seagulls in this beach-themed tower defense game!',
+    url: 'http://excaliburjs.com/ludum-50/',
+    source: 'https://github.com/excaliburjs/ludum-50',
+  },
+  {
+    title: '드롭다운',
+    Svg: dropdownImage,
+    description:
+      'This game was created for Ludum Dare 48. Dig deeply and quickly to get to your next metal gig, but watch out for the mechanical snake that pursues you!',
+    url: 'http://excaliburjs.com/ludum-48/',
+    source: 'https://github.com/excaliburjs/ludum-48',
+  },
+  {
+    title: '리스트 그룹',
+    Svg: listGroupImage,
+    description:
+      'This game was created for Ludum Dare 46. Keep the theater production going as the only actor!',
+    url: 'http://excaliburjs.com/ludum-46/',
+    source: 'https://github.com/excaliburjs/ludum-46',
+  },
+  {
+    title: '탭',
+    Svg: tabsImage,
+    description:
+      'This game was created for Ludum Dare 41. Balance the needs of your job with the needs of your dream!',
+    url: 'http://excaliburjs.com/ludum-41/',
+    source: 'https://github.com/excaliburjs/ludum-41',
+  },
+];
---
+++ src/pages/components/index.tsx
@@ -0,0 +1,65 @@
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+import data from './_data';
+
+function ComponentsPageHeader() {
+  return (
+    <header>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          컴포넌트
+        </Heading>
+        <p className="hero__subtitle">
+          기준이 되는 <code>컴포넌트</code> 모양과 사용법을 제시합니다.
+        </p>
+      </div>
+    </header>
+  );
+}
+
+const CardList = ({ items }) => (
+  <div className={styles.cards}>
+    {items.map(({ Svg, title, description, url, source }) => (
+      <div className={styles.card} key={title}>
+        <div className={styles.image}>
+          <Svg role="img" style={{ height: 180, width: 260 }} />
+        </div>
+        <div className={styles.content}>
+          <h3 className="header">{title}</h3>
+
+          <p className="description">{description}</p>
+        </div>
+        <div className={clsx(styles.extra, styles.content)}>
+          <a href={url}>Demo</a>
+          {!!source && (
+            <span className={clsx('right floated')}>
+              <a href={source}>Code</a>
+            </span>
+          )}
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+export default function Components(): JSX.Element {
+  return (
+    <Layout
+      title="컴포넌트"
+      description="Description will go into a meta tag in <head />"
+    >
+      <ComponentsPageHeader />
+      <main className="bg-black">
+        <div className="ui container">
+          <h2 className="ui header">컴포넌트</h2>
+          <div className="ui section divider" />
+          <CardList items={data} />
+          <div className="ui hidden divider" />
+        </div>
+      </main>
+    </Layout>
+  );
+}
