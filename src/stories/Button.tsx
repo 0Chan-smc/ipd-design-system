b0c783bed189e6bb927dac22d07c8bce89a76e63
0Chan-smc <cody@thesmc.co.kr>
2024-06-13 15:34:56 +0900

    feat: 심미성 평가를 위한 대조군 Button 컴포넌트 생성
    <Button/> 과 <Button2/> 컴포넌트 생성

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './button.css';
+import { Edit2 } from 'react-feather';
+
+import { cn } from '../utils';
 
 interface ButtonProps {
   /**
@@ -39,11 +41,46 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      // className={['font-sans','storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={cn('font-sans flex items-center justify-center px-4 py-2 text-xl rounded-3xl border-0 cursor-pointer')}
       style={{ backgroundColor }}
       {...props}
     >
+      <Edit2 className="w-5 h-5 mr-0.5"/>
       {label}
     </button>
   );
 };
+
+{/*
+  .storybook-button {
+    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 3em;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+  }
+  .storybook-button--primary {
+    color: white;
+    background-color: #1ea7fd;
+  }
+  .storybook-button--secondary {
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  }
+  .storybook-button--small {
+    font-size: 12px;
+    padding: 10px 16px;
+  }
+  .storybook-button--medium {
+    font-size: 14px;
+    padding: 11px 20px;
+  }
+  .storybook-button--large {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
+*/}
---
+++ src/stories/Button2.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Edit2 } from 'react-feather';
+
+import { cn } from '../utils';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+export const Button2 = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'bg-yellow-200' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={cn(
+        'font-sans flex items-center justify-center px-4 py-2 text-xl text-white border border-blue-400 rounded-lg cursor-pointer',
+        'bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500',
+        'hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-200',
+      )}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      <Edit2 className="w-5 h-5 mr-0.5"/>
+      {label}
+    </button>
+  );
+};
+
+{/* <button
+type="button"
+class="ml-auto flex items-center h-10 text-white bg-gradient-to-r from-blue-30
+0 via-blue-400 to-blue-500 border border-blue-400 hover:bg-gradient-to-br focus:ring-1
+ focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-700 font-medium rounded-lg px-3 py-2.5 text-center"
+onclick="location.href='/notice/notice_write.php'"
+>
+<i data-feather="edit-2" class="w-3.5 h-3.5"></i>
+<span class="ml-1">작성</span>
+</button> */}
